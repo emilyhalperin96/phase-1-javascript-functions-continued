cfda569bb13e3b4e3eea689b59b4b979713a853a
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-01-10 14:27:28 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun()); 
+  console.log(saturdayFun("go to the beach")); 
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork());
+console.log(mondayWork("go to the beach"));
+  //The function should return a String like "This Monday, I will ... ."
+  //Fill in the ... with the activity that's passed in as the first parameter. 
+  // If nothing is passed in, default to "go to the office"
+
+function wrapAdjective(result = "*") {
+    const innerFunction = function(adjective = "special") {
+        return `You are ${result}${adjective}${result}!`
+    };
+    return innerFunction;
+}
